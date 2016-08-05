f626bcf69c99f54494e8be32486f6167d80d19b5
Wojtek Czekalski <me@wczekalski.com>
2016-08-05 20:48:08 +0200

    Initial Commit

---
+++ TimeBudget/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TimeBudget
+//
+//  Created by Wojciech Czekalski on 05.08.2016.
+//  Copyright © 2016 wczekalski. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TimeBudget/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TimeBudget
+//
+//  Created by Wojciech Czekalski on 05.08.2016.
+//  Copyright © 2016 wczekalski. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
